d06cdf62dcfe322b06a0fa609033c21f9908585d
AiceKerime <rafiizzaturohman06@gmail.com>
2022-10-06 14:12:51 +0700

    Add & Delete DONE

--- database/data.sql
+++ database/data.sql
@@ -3,8 +3,8 @@ CREATE TABLE bread(
     string VARCHAR(50) NOT NULL,
     integer INTEGER NOT NULL,
     float REAL NOT NULL,
-    date DATE NOT NULL,
-    boolean BOOLEAN
+    date DATE,
+    boolean BOOLEAN NOT NULL
 );
 
 INSERT INTO bread (string, integer, float, date, boolean) VALUES ('Testing Data Euy', 12, 1.45, '2017-12-12', true);
