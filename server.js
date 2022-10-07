3aeafb9955cbb74422e1890edc125b9089649603
AiceKerime <rafiizzaturohman06@gmail.com>
2022-10-07 08:58:28 +0700

    CRUD DONE

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ const sqlite3 = require('sqlite3').verbose()
 // const dataPath = './json/data.json'
 // const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
 
+// DATABASE CONNECTING
 const dbFile = './database/data.db'
 const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE, (err) => {
   if (err) { console.log(`Failed to connect to database`, err) };
 });
 
+// EXPRESS & PORT
 const app = express()
 const port = 3000
 
 // USE
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')))
+
+// BODYPARSER
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -73,12 +77,13 @@ app.post('/add', (req, res) => {
 })
 
 app.post('/edit/:id', (req, res) => {
-  db.all('UPDATE FROM bread SET string = ?, integer = ?, float = ?, date = ?, boolean = ? WHERE id = ?', [req.body.string, parseInt(req.body.integer), parseFloat(req.body.float), req.body.date, JSON.parse(req.body.boolean), req.body.id], (err, data) => {
-    if (err) {
-      console.log('Failed to update data')
-    }
-    res.render('edit', { item: data })
-  })
+  db.run('UPDATE bread SET string = ?, integer = ?, float = ?, date = ?, boolean = ? WHERE id = ?',
+    [req.body.string, parseInt(req.body.integer), parseFloat(req.body.float), req.body.date, req.body.boolean, req.params.id], (err) => {
+      if (err) {
+        console.error('Failed to update data', err)
+      }
+      res.redirect('/');
+    })
 })
 
 
